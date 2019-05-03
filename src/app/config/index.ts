a3f677ea7c7b7cd859c224890725ae2f5980f4ae
safaldas <safaldas.g@neoito.com>
2019-05-03 17:10:27 +0530

    config file to store app constants

---
+++ src/app/config/index.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = "http://localhost:3000";
+
+export const POST_SENTENCE_URL = `${BASE_URL}/freq`;
+export const GET_WORDS_LIST_URL = `${BASE_URL}/user`;
+export const USER_URL = `${BASE_URL}/user`;
+
+export const LOCALSTORAGE_KEYS = {
+  userId: "user_id"
+};
