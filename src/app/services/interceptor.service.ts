f01e42c80cd78043ca2f850098f4b73547fa308c
safaldas <safaldas.g@neoito.com>
2019-05-03 17:09:33 +0530

    add interceptor to handle auth error

---
+++ src/app/services/interceptor.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from "@angular/common/http";
+import { tap } from "rxjs/operators";
+import { Observable } from "rxjs";
+
+import { UsersService } from "./user.service";
+
+@Injectable()
+export class Interceptor implements HttpInterceptor {
+  constructor(private userService: UsersService) {}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap(
+        success => {},
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            console.log(err);
+
+            if (err.status === 401 || err.status === 403) {
+              this.userService.create().subscribe(r => {
+                return next.handle(request);
+              });
+            } else if (err.status === 404) {
+            }
+          }
+        }
+      )
+    );
+  }
+}
